c9387b4e79fd51107367bb661c90cf75a35aa2da
Clementine DEROME <clementine.derome@gmail.com>
2020-05-05 13:43:45 +0200

    exercises checked: 1,2,3,4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,64 @@
 import React from 'react';
 import './App.css';
 
-const initialProductList = [
-  { id: 1, name: 'produit 1', price: 50, quantity: 1 },
-  { id: 2, name: 'produit 2', price: 75, quantity: 2 },
-  { id: 3, name: 'produit 3', price: 20, quantity: 5 }
-];
+// const initialProductList = [
+//   { id: 1, name: 'produit 1', price: 50, quantity: 1 },
+//   { id: 2, name: 'produit 2', price: 75, quantity: 2 },
+//   { id: 3, name: 'produit 3', price: 20, quantity: 5 }
+// ];
 
-function App () {
-  return (
-    <div className='App'>
-      <h1>Ma commande</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    ProductList: [
+      { id: 0, name: 'produit 1', price: 50, quantity: 1 },
+      { id: 1, name: 'produit 2', price: 75, quantity: 2 },
+      { id: 2, name: 'produit 3', price: 20, quantity: 5 }
+    ]
+  }
+
+    handleChange = (e, id) => {
+    const newValue = this.state.ProductList.slice()
+    newValue[id].quantity = e.target.value
+    this.setState(({ ProductList: newValue }))
+    if (e.target.value === '0') {
+      window.confirm('Are you sure to delete that ?')
+      const productListCopy = this.state.ProductList.slice()
+      const newProductList = productListCopy.filter((product) => product.quantity !== "0") 
+      console.log(newProductList)
+      this.setState(({ ProductList: newProductList }))
+    }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Ma commande</h1>
+        <table>
+          <thead>
+            <tr>
+              <td>Produit</td>
+              <td>Prix unitaire</td>
+              <td>Quantité</td>
+              <td>Prix total</td>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.ProductList.map(({ id, name, price, quantity }) => (
+              <tr key={id}>
+                <td className="name">{name}</td>
+                <td className="price">{price} €</td>
+                <td className="quantity"><input value={quantity} type="number" onChange={(e) => this.handleChange(e, id)}></input></td>
+                <td className="total">{price * quantity} €</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <p className="totalPrice">
+          Montant total : {this.state.ProductList.map((product) => product.price * product.quantity).reduce((a, b) => a + b)} €
+          </p>
+      </div>
+    );
+  }
 }
 
 export default App;
